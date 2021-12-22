8f7538400fbda74e483c18908f1fa8bf4bb56f8c
Krystian Jakusik <kyrstke@gmail.com>
2021-12-22 11:40:21 +0100

    Update ToUpper.java

--- src/main/java/pl/put/poznan/transformer/logic/ToUpper.java
+++ src/main/java/pl/put/poznan/transformer/logic/ToUpper.java
@@ -1,18 +1,38 @@
 package pl.put.poznan.transformer.logic;
 
-public class ToUpper extends TextTransformer{
-
-    public ToUpper(IText transInterface) {
-        super(transInterface);
+/**
+ * Class changing all the characters to UPPERCASE
+ *
+ * @author Jan Kabzinski
+ * @version 1.0
+ */
+public class ToUpper extends TextTransformer {
+    /**
+     * Constructor of text transformation class.
+     *
+     * @param textInput text to decorate
+     */
+    public ToUpper(IText textInput) {
+        super(textInput);
     }
 
+    /**
+     * Method performing the transformation
+     *
+     * @return text after transformation
+     */
     @Override
-    public String transform(String text){
-        return toUpper(text);
+    public String transform() {
+        return toUpper(super.transform());
     }
 
-    private String toUpper(String text)
-    {
+    /**
+     * Method converting given text to UPPERCASE
+     *
+     * @param text given string by user
+     * @return text after transformation
+     */
+    private String toUpper(String text) {
         return text.toUpperCase();
     }
 }
