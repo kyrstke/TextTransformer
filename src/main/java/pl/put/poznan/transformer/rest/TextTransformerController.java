f88eae62c291ba1127d0e1fab8772aeefb4b2268
Filip <fil.kokosza@gmail.com>
2021-12-22 02:20:16 +0100

    json output input done

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,66 +1,48 @@
 package pl.put.poznan.transformer.rest;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.json_format.trans_info;
 import pl.put.poznan.transformer.logic.TextTransformer;
 
 import java.util.Arrays;
-import java.util.Objects;
 
+import org.json.JSONObject;
 
 @RestController
-@RequestMapping("/{trans}/{text}")
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String trans, @PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        if(Objects.equals(trans, "upper")) {
-            // perform the transformation, you should run your logic here, below is just a silly example
-            TextTransformer transformer = new TextTransformer(transforms);
-            return transformer.transform(text);
-        }
-        else if (Objects.equals(trans, "inverse"))
-        {
-            TextTransformer transformer = new TextTransformer(transforms);
-            return transformer.transform(text);
-        }
-        else
-        return null;
-    }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable  String trans, @PathVariable String text,
-                      @RequestBody String[] transforms) {
+    @RequestMapping(value ="/TextTransform/{text}", method = RequestMethod.GET, produces = "application/json")
+    public String post(@PathVariable String text,
+                        @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
         // log the parameters
         logger.debug(text);
         logger.debug(Arrays.toString(transforms));
 
         // perform the transformation, you should run your logic here, below is just a silly example
-        if(Objects.equals(trans, "upper")) {
-            // perform the transformation, you should run your logic here, below is just a silly example
-            TextTransformer transformer = new TextTransformer(transforms);
-            return transformer.transform(text);
-        }
-        else if (Objects.equals(trans, "inverse"))
-        {
-            TextTransformer transformer = new TextTransformer(transforms);
-            return transformer.transform(text);
-        }
-        else
-            return null;
+        TextTransformer transformer = new TextTransformer(transforms);
+        return transformer.transform(text);
     }
 
+    @RequestMapping(value = "/TextTransform", method = RequestMethod.POST, produces = "application/json")
+    public String get(@RequestBody trans_info transInfo) throws JSONException {
 
+        // log the parameters
+        logger.debug(transInfo.getText());
+        logger.debug(Arrays.toString(transInfo.getTransforms()));
 
+        // perform the transformation, you should run your logic here, below is just a silly example
+        TextTransformer transformer = new TextTransformer(transInfo.getTransforms());
+
+        JSONObject response = new JSONObject();
+        response.put("text", transformer.transform(transInfo.getText()));
+
+        return response.toString();
+    }
 }
 
 
