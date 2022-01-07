3a42407b7763a80c85f2a0e3e521482b103aa6f6
unknown <anastasiia.trubchaninova@student.put.poznan.pl>
2022-01-07 21:27:13 +0100

    test commit

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
     }
