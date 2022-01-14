a6789bd25f99abbf4a0c359e142fcf8f79a7ce03
kyrstke <kyrstke@gmail.com>
2022-01-14 22:07:06 +0100

    Added logic of ToAbbreviation and ExpandAbbreviation, deleted Abbreviations class

--- src/main/java/pl/put/poznan/transformer/logic/ExpandAbbreviation.java
+++ src/main/java/pl/put/poznan/transformer/logic/ExpandAbbreviation.java
@@ -1,10 +1,13 @@
 package pl.put.poznan.transformer.logic;
 
+import java.lang.Character;
+
 public class ExpandAbbreviation extends TextTransformer{
-    Abbreviations abbreviations;
+
     public ExpandAbbreviation (IText transInterface) {
         super(transInterface);
     }
+
     @Override
     public String transform(String text){
 
@@ -13,6 +16,32 @@ public class ExpandAbbreviation extends TextTransformer{
 
     private String expandAbbreviation(String text)
     {
-        return text;
+        String[] abbreviations = {"prof.", "dr", "np.", "itd.", "itp.", "nr", "mgr"};
+        String[] expansions = {"profesor", "doktor", "na przykład", "i tak dalej", "i tym podobne", "numer", "magister"};
+        String[] words = text.split(" ");
+        StringBuilder newText = new StringBuilder();
+
+
+        for(int i = 0; i < abbreviations.length; i++)
+        {
+            for(int j = 0; j < words.length; j++){
+                if(words[j].toLowerCase().equals(abbreviations[i])){
+                    if(Character.isUpperCase(words[j].charAt(0))){
+                        words[j] = expansions[i].substring(0, 1).toUpperCase() + expansions[i].substring(1);
+                    }
+                    else{
+                        words[j] = expansions[i];
+                    }
+
+                }
+            }
+        }
+
+        for(int j = 0; j < words.length; j++) {
+            if (j == 0) newText.append(words[j]);
+            else newText.append(" ").append(words[j]);
+        }
+
+        return newText.toString();
     }
 }
--- src/main/java/pl/put/poznan/transformer/logic/ToAbbreviation.java
+++ src/main/java/pl/put/poznan/transformer/logic/ToAbbreviation.java
@@ -1,17 +1,85 @@
 package pl.put.poznan.transformer.logic;
 
+import java.lang.Character;
+
 public class ToAbbreviation extends TextTransformer{
-    Abbreviations abbreviations;
-    public ToAbbreviation(IText transInterface) {
-        super(transInterface);
-    }
+
+    public ToAbbreviation(IText transInterface) { super(transInterface); }
+
     @Override
     public String transform(String text){
+
         return toAbbreviation(this.transInterface.transform(text));
     }
 
     private String toAbbreviation(String text)
     {
-        return text;
+        String[] expansions = {"profesor", "doktor", "na przykład", "i tak dalej", "i tym podobne", "numer", "magister"};
+        String[] abbreviations = {"prof.", "dr", "np.", "itd.", "itp.", "nr", "mgr"};
+        String[] words = text.split(" ");
+        StringBuilder newText = new StringBuilder();
+
+
+        for(int i = 0; i < expansions.length; i++)
+        {
+            for(int j = 0; j < words.length; j++){
+                if(words[j].toLowerCase().equals(expansions[i])){
+                    if(Character.isUpperCase(words[j].charAt(0))){
+                        words[j] = abbreviations[i].substring(0, 1).toUpperCase() + abbreviations[i].substring(1);
+                    }
+                    else{
+                        words[j] = abbreviations[i];
+                    }
+
+                }
+                else if(words[j].toLowerCase().equals(expansions[3].substring(0, 1))){
+                    if(words[j+1].toLowerCase().equals(expansions[3].substring(2, 5))){
+                        if(words[j+2].toLowerCase().equals(expansions[3].substring(6))) {
+                            if (Character.isUpperCase(words[j].charAt(0))) {
+                                words[j] = abbreviations[3].substring(0, 1).toUpperCase() + abbreviations[3].substring(1);
+                            } else {
+                                words[j] = abbreviations[3];
+                            }
+                            words[j+1] = "";
+                            words[j+2] = "";
+                            j+=2;
+                        }
+                    }
+                    else if(words[j+1].toLowerCase().equals(expansions[4].substring(2, 5))){
+                        if(words[j+2].toLowerCase().equals(expansions[4].substring(6))) {
+                            if (Character.isUpperCase(words[j].charAt(0))) {
+                                words[j] = abbreviations[4].substring(0, 1).toUpperCase() + abbreviations[4].substring(1);
+                            } else {
+                                words[j] = abbreviations[4];
+                            }
+                            words[j+1] = "";
+                            words[j+2] = "";
+                            j+=2;
+                        }
+                    }
+                }
+                else if(words[j].toLowerCase().equals(expansions[2].substring(0,2))){
+                    System.out.println("Wykryto: na");
+                    if(words[j+1].toLowerCase().equals(expansions[2].substring(3))){
+                        System.out.println("Wykryto: przykład");
+                        if(Character.isUpperCase(words[j].charAt(0))){
+                            words[j] = abbreviations[2].substring(0, 1).toUpperCase() + abbreviations[2].substring(1);
+                        }
+                        else{
+                            words[j] = abbreviations[2];
+                        }
+                        words[j+1] = "";
+                        j++;
+                    }
+                }
+            }
+        }
+
+        for(int j=0; j<words.length; j++) {
+            if (j==0 || words[j].equals("")) newText.append(words[j]);
+            else newText.append(" ").append(words[j]);
+        }
+
+        return newText.toString();
     }
 }
