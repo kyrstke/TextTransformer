809256e731b0159f1ebec064fa3efe4d8fd84e6d
kyrstke <kyrstke@gmail.com>
2022-01-15 13:12:18 +0100

    Added ToAbbreviationTest.java and debugged ToAbbreviation

--- src/main/java/pl/put/poznan/transformer/logic/ToAbbreviation.java
+++ src/main/java/pl/put/poznan/transformer/logic/ToAbbreviation.java
@@ -2,84 +2,95 @@ package pl.put.poznan.transformer.logic;
 
 import java.lang.Character;
 
-public class ToAbbreviation extends TextTransformer{
+public class ToAbbreviation extends TextTransformer {
 
-    public ToAbbreviation(IText transInterface) { super(transInterface); }
+    public ToAbbreviation(IText transInterface) {
+        super(transInterface);
+    }
 
     @Override
-    public String transform(String text){
+    public String transform(String text) {
 
         return toAbbreviation(this.transInterface.transform(text));
     }
 
-    private String toAbbreviation(String text)
-    {
+    private String toAbbreviation(String text) {
         String[] expansions = {"profesor", "doktor", "na przykład", "i tak dalej", "i tym podobne", "numer", "magister"};
         String[] abbreviations = {"prof.", "dr", "np.", "itd.", "itp.", "nr", "mgr"};
         String[] words = text.split(" ");
         StringBuilder newText = new StringBuilder();
+        boolean comma = false, dot = false;
 
 
-        for(int i = 0; i < expansions.length; i++)
-        {
-            for(int j = 0; j < words.length; j++){
-                if(words[j].toLowerCase().equals(expansions[i])){
-                    if(Character.isUpperCase(words[j].charAt(0))){
-                        words[j] = abbreviations[i].substring(0, 1).toUpperCase() + abbreviations[i].substring(1);
-                    }
-                    else{
-                        words[j] = abbreviations[i];
-                    }
-
-                }
-                else if(words[j].toLowerCase().equals(expansions[3].substring(0, 1))){
-                    if(words[j+1].toLowerCase().equals(expansions[3].substring(2, 5))){
-                        if(words[j+2].toLowerCase().equals(expansions[3].substring(6))) {
-                            if (Character.isUpperCase(words[j].charAt(0))) {
-                                words[j] = abbreviations[3].substring(0, 1).toUpperCase() + abbreviations[3].substring(1);
-                            } else {
-                                words[j] = abbreviations[3];
-                            }
-                            words[j+1] = "";
-                            words[j+2] = "";
-                            j+=2;
-                        }
-                    }
-                    else if(words[j+1].toLowerCase().equals(expansions[4].substring(2, 5))){
-                        if(words[j+2].toLowerCase().equals(expansions[4].substring(6))) {
-                            if (Character.isUpperCase(words[j].charAt(0))) {
-                                words[j] = abbreviations[4].substring(0, 1).toUpperCase() + abbreviations[4].substring(1);
-                            } else {
-                                words[j] = abbreviations[4];
-                            }
-                            words[j+1] = "";
-                            words[j+2] = "";
-                            j+=2;
-                        }
-                    }
-                }
-                else if(words[j].toLowerCase().equals(expansions[2].substring(0,2))){
-                    System.out.println("Wykryto: na");
-                    if(words[j+1].toLowerCase().equals(expansions[2].substring(3))){
-                        System.out.println("Wykryto: przykład");
-                        if(Character.isUpperCase(words[j].charAt(0))){
-                            words[j] = abbreviations[2].substring(0, 1).toUpperCase() + abbreviations[2].substring(1);
-                        }
-                        else{
-                            words[j] = abbreviations[2];
-                        }
-                        words[j+1] = "";
-                        j++;
-                    }
-                }
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].endsWith(",")) {
+                words[i] = words[i].substring(0, words[i].length() - 1);
+                comma = true;
+                System.out.println(words[i]);
+            } else if (words[i].endsWith(".")) {
+                words[i] = words[i].substring(0, words[i].length() - 1);
+                dot = true;
+                System.out.println(words[i]);
             }
-        }
+            for (int j = 0; j < expansions.length; j++) {
+                if (words[i].toLowerCase().equals(expansions[j])) {
+                    if (Character.isUpperCase(words[i].charAt(0))) {
+                        words[i] = abbreviations[j].substring(0, 1).toUpperCase() + abbreviations[j].substring(1);
+                    } else {
+                        words[i] = abbreviations[j];
+                    }
+                    break;
+                } else if (words[i].toLowerCase().equals(expansions[3].substring(0, 1))) {
+                    if (words[i + 1].toLowerCase().equals(expansions[3].substring(2, 5))) {
+                        System.out.println(expansions[3].substring(6));
+                        if (words[i + 2].toLowerCase().equals(expansions[3].substring(6))) {
+                            if (Character.isUpperCase(words[i].charAt(0))) {
+                                words[i] = abbreviations[3].substring(0, 1).toUpperCase() + abbreviations[3].substring(1);
+                            } else {
+                                words[i] = abbreviations[3];
+                            }
+                            words[i + 1] = "";
+                            words[i + 2] = "";
+                            break;
+                        }
+                    } else if (words[i + 1].toLowerCase().equals(expansions[4].substring(2, 5))) {
+                        if (words[i + 2].toLowerCase().equals(expansions[4].substring(6))) {
+                            if (Character.isUpperCase(words[i].charAt(0))) {
+                                words[i] = abbreviations[4].substring(0, 1).toUpperCase() + abbreviations[4].substring(1);
+                            } else {
+                                words[i] = abbreviations[4];
+                            }
+                            words[i + 1] = "";
+                            words[i + 2] = "";
+                            break;
+                        }
+                    }
+                } else if (words[i].toLowerCase().equals(expansions[2].substring(0, 2))) {
+                    if (words[i + 1].toLowerCase().equals(expansions[2].substring(3))) {
+                        if (Character.isUpperCase(words[i].charAt(0))) {
+                            words[i] = abbreviations[2].substring(0, 1).toUpperCase() + abbreviations[2].substring(1);
+                        } else {
+                            words[i] = abbreviations[2];
+                        }
+                        words[i + 1] = "";
+                        break;
+                    }
+                }
 
-        for(int j=0; j<words.length; j++) {
-            if (j==0 || words[j].equals("")) newText.append(words[j]);
-            else newText.append(" ").append(words[j]);
+            }
+            if (comma) {
+                if (i == 0 || words[i].equals("")) newText.append(words[i]).append(",");
+                else newText.append(" ").append(words[i]).append(",");
+            } else if (dot) {
+                if (i == 0 || words[i].equals("")) newText.append(words[i]).append(".");
+                else newText.append(" ").append(words[i]).append(".");
+            } else {
+                if (i == 0 || words[i].equals("")) newText.append(words[i]);
+                else newText.append(" ").append(words[i]);
+            }
+            comma = false;
+            dot = false;
         }
-
         return newText.toString();
     }
 }
---
+++ src/test/java/pl/put/poznan/transformer/logic/ToAbbreviationTest.java
@@ -0,0 +1,147 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+class ToAbbreviationTest {
+
+        private IText textTransformer;
+
+        @BeforeEach
+        public void setupTextTransformer()
+        {
+            textTransformer = new ToAbbreviation(new InputString());
+        }
+
+        @Test
+        public void test1()
+        {
+            String text = "Każdy doktor był kiedyś magistrem.";
+            String result = "Każdy dr był kiedyś magistrem.";
+
+            Assertions.assertEquals(result, textTransformer.transform(text));
+        }
+
+        @Test
+        public void test2()
+        {
+            String text = "Magister, na przykład Filip to nie DOKTOR.";
+            String result = "Mgr, np. Filip to nie Dr.";
+
+            Assertions.assertEquals(result,  textTransformer.transform(text));
+        }
+
+        @Test
+        public void test3()
+        {
+            String text = "Numer telefonu Doktora to 997 420 i tak dalej";
+            String result = "Nr telefonu Doktora to 997 420 itd.";
+
+            Assertions.assertEquals(result, textTransformer.transform(text));
+        }
+
+//        @Test
+//        public void testZero()
+//        {
+//            String text = "zero to 0 a 0 to zero";
+//            String result = "zero to zero a zero to zero";
+//
+//            Assertions.assertEquals(result, textTransformer.transform(text));
+//        }
+//
+//        @Test
+//        public void testMultipleNumInText()
+//        {
+//            String text = "8.12 + 4.03 = 12.15";
+//            String result = "osiem i dwanaście setnych + cztery i trzy setne = dwanaście i piętnaście setnych";
+//
+//            Assertions.assertEquals(result, textTransformer.transform(text));
+//        }
+//
+//        @Test
+//        public void testNegativeNumInText()
+//        {
+//            String text = "-0.3 jest jak -0.03 ale nie do końca";
+//            String result ="minus trzy dziesiąte jest jak minus trzy setne ale nie do końca";
+//
+//            Assertions.assertEquals(result, textTransformer.transform(text));
+//        }
+//
+//        @Test
+//        public void testLongNumbers()
+//        {
+//            String text = "999.99 to prawie 1000 tak jak -1000 to prawie -999.99";
+//            String result = "dziewięćset dziewięćdziesiąt dziewięć i dziewięćdziesiąt dziewięć setnych to prawie tysiąc tak " +
+//                    "jak minus tysiąc to prawie minus dziewięćset dziewięćdziesiąt dziewięć i dziewięćdziesiąt dziewięć setnych";
+//
+//            Assertions.assertEquals(result, textTransformer.transform(text));
+//        }
+//
+//        /**
+//         * tests using mockito
+//         */
+//        @Test
+//        public void testMockRest()
+//        {
+//            IText iText = mock(IText.class);
+//
+//            when(iText.transform("32.01")).thenReturn("trzydzieści dwa i jedna setna");
+//
+//            NumbersToText text = new NumbersToText(iText);
+//            String result = text.transform("32.01");
+//            verify(iText).transform("32.01");
+//
+//            assertEquals("trzydzieści dwa i jedna setna", result);
+//        }
+//
+//        @Test
+//        public void testMockNegative()
+//        {
+//            IText iText = mock(IText.class);
+//
+//            when(iText.transform("-0.03")).thenReturn("minus trzy setne");
+//
+//            NumbersToText text = new NumbersToText(iText);
+//            String result = text.transform("-0.03");
+//            verify(iText).transform("-0.03");
+//
+//            assertEquals("minus trzy setne", result);
+//        }
+//
+//        @Test
+//        public void testMockMultipleTrans()
+//        {
+//            IText iText = mock(IText.class);
+//
+//            when(iText.transform("janusz ma 32.03 zlotych na konica a halyna -3.99 euro")).thenReturn("janusz ma trzydzieście dwa i trzy setne zlote na konica a " +
+//                    "halyna minus trzy i dziewięćdziesiąt dziewięć setnych euro");
+//
+//            NumbersToText text = new NumbersToText(iText);
+//            String result = text.transform("janusz ma 32.03 zlotych na konica a halyna -3.99 euro");
+//            verify(iText).transform("janusz ma 32.03 zlotych na konica a halyna -3.99 euro");
+//
+//            assertEquals("janusz ma trzydzieście dwa i trzy setne zlote na konica a " +
+//                    "halyna minus trzy i dziewięćdziesiąt dziewięć setnych euro", result);
+//        }
+//
+//        @Test
+//        public void testMockAboveBelowLimit()
+//        {
+//            IText iText = mock(IText.class);
+//
+//            when(iText.transform("10000 w portfelu i -1324.345 na koncie")).thenReturn("10000 w portfelu i -1324.345 na koncie");
+//
+//            NumbersToText text = new NumbersToText(iText);
+//            String result = text.transform("10000 w portfelu i -1324.345 na koncie");
+//            verify(iText).transform("10000 w portfelu i -1324.345 na koncie");
+//
+//            assertEquals("10000 w portfelu i -1324.345 na koncie", result);
+//        }
+//
+    }
+//}
